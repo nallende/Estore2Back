98a94a06c36f5a58298d3b6fe120b9c57d166ab6
nallende <nallende@neosoporte.cl>
2021-11-10 13:57:05 -0300

    fy10

--- Routes/product.js
+++ Routes/product.js
@@ -4,26 +4,14 @@ const router = express.Router();
 const { categories } = require("../models");
 
 router.get("/getCategories", async (req, res) => {
-  let appData = {
-    isError: false,
-    data: [],
-  };
-  await categories.findAll((error, ...rows) => {
-    if (error) {
-      appData.isError = true;
-      appData.data = err;
-      res.status(500).json(appData);
-    } else {
-      appData.data = rows;
-      res.status(200).json(appData);
-    }
-  });
+  const appData = await categories.findAll();
+  res.json(appData);
 });
 
 const { products } = require("../models");
 
 router.get("/getProducts", async (req, res) => {
-  const product = await products.findByPk();
+  const product = await products.findAll();
   res.json(product);
 });
 
