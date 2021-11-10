db953fe2b433820a63f38b2928b4fdac60cedb16
nallende <nallende@neosoporte.cl>
2021-11-10 13:51:00 -0300

    futbol2

--- Routes/product.js
+++ Routes/product.js
@@ -4,8 +4,20 @@ const router = express.Router();
 const { categories } = require("../models");
 
 router.get("/getCategories", async (req, res) => {
-  const appData = await categories.findAll();
-  res.json(appData);
+  let appData = {
+    isError: false,
+    data: [],
+  };
+  await categories.findAll((error, ...rows) => {
+    if (error) {
+      appData.isError = true;
+      appData.data = err;
+      res.status(500).json(appData);
+    } else {
+      appData.data = rows;
+      res.status(200).json(appData);
+    }
+  });
 });
 
 const { products } = require("../models");
